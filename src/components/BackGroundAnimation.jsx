70f9b14f9fa671bc7f8f42f4de105089c9a26d96
amberjackman <feverhong@vivaldi.net>
2024-06-27 21:04:38 +0900

    animate fix7

--- src/components/BackGroundAnimation.jsx
+++ src/components/BackGroundAnimation.jsx
@@ -25,7 +25,7 @@ const FloatingDot = styled.div`
 const BackgroundAnimationContainer = styled.div`
   position: absolute;
   top: 0;
-  margin-left: -700px;
+  left: 0;
   width: 100%;
   height: 100%;
   /* overflow: hidden; */
