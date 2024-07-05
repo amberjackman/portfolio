e4f8a957a63334eb22fd221c4460104344d78262
amberjackman <feverhong@vivaldi.net>
2024-07-05 21:39:52 +0900

    contact 정리

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { useSpring, animated, fadeIn } from "react-spring";
+import { useSpring, animated } from "react-spring";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const ContactSection = styled(animated.section)`
   padding: 2rem;
@@ -9,43 +10,113 @@ const ContactSection = styled(animated.section)`
 const ContactContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  text-align: center;
 `;
 
-const Form = styled.form`
+const IconContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
-const Input = styled.input`
-  padding: 0.5rem;
-  border: 1px solid #ddd;
+const Tooltip = styled.span`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 75%;
+
+  height: 50%;
+  transform: translateY(50%);
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  white-space: normal;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+  pointer-events: none;
+  z-index: 20;
+  word-wrap: break-word;
 `;
 
-const Button = styled.button`
-  background: #333;
-  color: #fff;
-  border: none;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  transition: background 0.3s;
-
-  &:hover {
-    background: #555;
+const IconBox = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 8rem;
+  height: 8rem;
+  margin: 1rem;
+  /* border: 1px solid red; */
+  @media (max-width: 768px) {
+    margin: 0;
+  }
+  img {
+    cursor: pointer;
+    width: 50%;
+    height: 50%;
+  }
+  &:hover ${Tooltip} {
+    visibility: visible;
+    opacity: 1;
   }
 `;
 
 function Contact() {
+  const [copied, setCopied] = useState(false);
   const fadeIn = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     config: { duration: 1000 },
   });
 
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1000);
+  };
+
   return (
     <ContactSection style={fadeIn} id="contact">
       <ContactContainer>
         <h2>Contact</h2>
+        <IconContainer>
+          <CopyToClipboard text="01028954797" onCopy={handleCopy}>
+            <IconBox>
+              <img src="/images/telephone.png" alt="phone" />
+              <Tooltip>
+                {copied ? "복사됨!" : " 01028954797  클릭 시 복사됩니다!"}
+              </Tooltip>
+            </IconBox>
+          </CopyToClipboard>
+          <CopyToClipboard text="feverhong@naver.com " onCopy={handleCopy}>
+            <IconBox>
+              <img src="/images/email.png" alt="email" />
+              <Tooltip>
+                {copied
+                  ? "복사됨!"
+                  : "feverhong@naver.com  클릭 시 복사됩니다!"}
+              </Tooltip>
+            </IconBox>
+          </CopyToClipboard>
+          <IconBox>
+            <img src="/images/kakao-talk.png" alt="kakaotalk" />
+            <Tooltip>클릭 시 연결됩니다!</Tooltip>
+          </IconBox>
+          <IconBox>
+            <a
+              href="https://github.com/amberjackman"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/images/github.png" alt="github" />
+            </a>
+            <Tooltip>클릭 시 이동합니다!</Tooltip>
+          </IconBox>
+        </IconContainer>
       </ContactContainer>
     </ContactSection>
   );
